d77e099636fb2b8c331a5421e84ac1baf0002c67
Laptseu <alexlaptseu1@Alexs-MacBook-Pro.local>
2019-05-01 17:14:50 +1000

    added feature

--- README.md
+++ README.md
@@ -1,3 +1,28 @@
-##### Insomnia json file template tag which changes content of json file if keywords match with environment variables
+# Insomnia json file template tag 
+This plugin changes content of json file if keywords match with environment variables
 
-IN PROGRESS ...
+Compulsory environment variable used by this insomnia plugin is **json_property** has json structure:
+
+```
+{
+"json_property": {
+    "version": "1.0"
+  }
+}
+ ```
+ 
+ ### To install plugin manually
+  1) Checkout this repository;
+  2) Copy source code to insomnia plugins directory(see paths below);
+  3) Create environment variables(see structure above) with your values; 
+  
+  - ___MacOS_:__ ~/Library/Application\ Support/Insomnia/plugins/
+  
+  - _**Windows**_: %APPDATA%\Insomnia\plugins\
+  
+  - _**Linux**_: $XDG_CONFIG_HOME/Insomnia/plugins/ or ~/.config/Insomnia/plugins/
+  
+  
+  ### Todos
+   - Write Tests
+   - Publish plugin to [npm](https://www.npmjs.com/)
--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const fs = require('fs');
+const editJsonFile = require("edit-json-file");
 
 module.exports.templateTags = [
     {
@@ -11,12 +12,25 @@ module.exports.templateTags = [
                 type: 'file',
             }
         ],
-        run(context, path) {
+        run(ctx, path) {
             if (!path) {
-                throw new Error('No file selected');
+                throw new Error('No JSON file selected');
             }
-
-            return fs.readFileSync(path, 'utf8');
+            const json_values = ctx.context.json_property;
+            if (!json_values) {
+                return  fs.readFileSync(path, 'utf8');
+            }
+            let file_content = editJsonFile(path);
+            console.log(`File content before setting up values:`);
+            console.log(file_content.get());
+            for (const key of Object.keys(json_values)) {
+                const value = json_values[key];
+                console.log(`Processing key: ${key} with value: ${value}`);
+                file_content.set(key, value);
+            }
+            console.log(`File content after setting up values:`);
+            console.log(file_content.get());
+            return JSON.stringify(file_content.get());
         },
     },
 ];
